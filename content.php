0d251e8d1b429a54c64b72f0ce9129f0ac3d98cf
tongdung13 <tongvanthienvu@gmail.com>
2020-11-06 14:30:35 +0700

    ok

---
+++ content.php
@@ -0,0 +1,13 @@
+
+<h1>London</h1>
+<p>fjsdfsdlfhsilfjw;jpoklsdfjklfjlsdhfslgj;g'f;jgjas</p>
+<p>sdkfjasdfljsd;lfh;fhsklfhsd;lfgs;gjs;ghaslfhaslfjs;'</p>
+
+<style>
+    article {
+        margin-left: 170px;
+        border-left: 1px solid gray;
+        padding: 1em;
+        overflow: hidden;
+    }
+</style>
---
+++ footer.php
@@ -0,0 +1,12 @@
+Copyright &copy; W3Schools.com
+
+
+<style>
+    header, footer {
+        padding: 1em;
+        color: white;
+        background-color: black;
+        clear: left;
+        text-align: center;
+    }
+</style>
